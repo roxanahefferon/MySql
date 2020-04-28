542d47262d5c8ec9f62c0ec8f02f72c47318cb9d
roxanahefferon <roxana.hefferon@gmail.com>
2020-04-28 10:59:03 +0000

    execute many, update and interpolation

--- mysql-update.py
+++ mysql-update.py
@@ -1,5 +1,4 @@
 import os
-import datetime
 import pymysql
 
 # Get the username from the Cloud9 workspace
@@ -11,12 +10,9 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
 try:
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                          Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the
-        # table already exists
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
+        connection.commit()
 finally:
     connection.close()
