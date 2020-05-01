b3575299c88f31aa7dcff49d00e58f3973e36638
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-01 10:12:41 +0000

    env

--- mongo.py
+++ mongo.py
@@ -1,6 +1,11 @@
 import pymongo
 import os
 
+from os import path
+
+if path.exists("env.py"):
+  import env 
+
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
